090bbc49d16a2cbc66a1765479298139c6c4df1e
Mihai Smarandache <mihailiviu@yahoo.com>
2018-04-11 20:18:13 -0700

    Created HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  userProfile : any;
 
-  constructor() { }
+  constructor(private router : Router, private userService : UserService) { }
 
   ngOnInit() {
+    /* this.userService.getUser().subscribe((data : any) => {
+      this.userProfile = data;
+    }); */
+  }
+
+  Logout(){
+    localStorage.removeItem('userToken');
+    this.router.navigate(['/login']);
   }
 
 }
