8a9c1a4c2815660556a3755fb920964c901066a8
Mihai Smarandache <mihailiviu@yahoo.com>
2018-04-11 20:29:45 -0700

    Final Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
 import { HomeComponent } from './home/home.component';
 import { appRoutes } from './routes';
-
+import { UserService } from './shared/user.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Http } from '@angular/http';
+import { AuthGuard } from './auth/auth.guard';
 
 @NgModule({
   declarations: [
@@ -19,9 +22,11 @@ import { appRoutes } from './routes';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService, Http, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
