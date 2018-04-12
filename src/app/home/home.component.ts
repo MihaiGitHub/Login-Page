c0cf9a2cfb69de8eafb43fdcba0d3ab1b87c0245
Mihai Smarandache <mihailiviu@yahoo.com>
2018-04-12 16:16:58 -0700

    Displaying user profile and deleting JSON Web Token on logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../shared/user.service';
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +9,16 @@ import { UserService } from '../shared/user.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  userProfile : any;
 
-  constructor(private router : Router, private userService : UserService) { }
+  constructor(private router : Router, private userService : UserService, private userProfile : User) { }
 
   ngOnInit() {
-    /* this.userService.getUser().subscribe((data : any) => {
+    this.userService.getUser().subscribe((data : any) => {  // Get user profile and storing it in User model
       this.userProfile = data;
-    }); */
+    });
   }
 
-  Logout(){
+  Logout(){  // Delete JWT on logout
     localStorage.removeItem('userToken');
     this.router.navigate(['/login']);
   }
