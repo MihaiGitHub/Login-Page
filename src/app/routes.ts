a08b535e6a3b0ef261d3c21efead2dd7800573ac
Mihai Smarandache <mihailiviu@yahoo.com>
2018-04-11 20:20:28 -0700

    Created routes file

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const appRoutes : Routes = [
-    { path : 'home', component : HomeComponent },
+    { path : 'home', component : HomeComponent, canActivate:[AuthGuard] },
     {
         path: 'login', component: UserComponent,
         children: [{ path: '', component: SignInComponent }]
